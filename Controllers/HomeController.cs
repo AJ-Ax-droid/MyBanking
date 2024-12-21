74fdb241ab4a5467fb87bf0e58ac1b21019c92d8
AJ-Ax-droid <mohanpandey8827@outlook.com>
2024-12-21 15:36:14 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBanking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+    }
+}
